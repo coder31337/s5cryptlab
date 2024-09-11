5ed242bf6c9e2946fd3efa28b8a7e78a3ae72686
coder31337 <coder31337@coder31337>
2024-09-11 16:26:22 +0000

    Add code and output of p6

---
+++ src/p6_des.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.security.MessageDigest;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.Cipher;
+
+class DES {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a key: ");
+            String key = scanner.nextLine();
+            byte[] keyBytes = key.getBytes("UTF8");
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] keyHash = md.digest(keyBytes);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey desKey = keyFactory.generateSecret(new DESKeySpec(keyHash));
+
+            System.out.print("Enter a message: ");
+            String message = scanner.nextLine();
+            byte[] messageBytes = message.getBytes("UTF8");
+
+            Cipher desCipher = Cipher.getInstance("DES");
+            desCipher.init(Cipher.ENCRYPT_MODE, desKey);
+            byte[] ciphertextBytes = desCipher.doFinal(messageBytes);
+            String ciphertext = new String(ciphertextBytes);
+            System.out.println("\nAfter encryption");
+            System.out.println("Ciphertext is " + ciphertext);
+
+            desCipher.init(Cipher.DECRYPT_MODE, desKey);
+            byte[] plaintextBytes = desCipher.doFinal(ciphertextBytes);
+            String plaintext = new String(plaintextBytes);
+            System.out.println("\nAfter decryption");
+            System.out.println("Plaintext is  " + plaintext);
+
+        } catch (Exception e) {
+            System.out.println("Error occurred");
+        }
+    }
+}
