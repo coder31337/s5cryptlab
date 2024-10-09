f8739eecf10e1013f901d5119773c605b1f9a138
coder31337 <coder31337@coder31337>
2024-10-09 14:43:48 +0000

    Add code and output of p11

---
+++ src/p11_hashing.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.security.MessageDigest;
+
+class Hashing {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            
+            System.out.print("Enter a message: ");
+            String message = scanner.nextLine();
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+            byte[] hash = md.digest(message.getBytes());
+            System.out.print("Hash is ");
+            for (byte b: hash)
+                System.out.print(String.format("%x", b));
+            System.out.println();
+
+        } catch (Exception e) {
+            System.out.println("Error occurred");
+        }
+    }
+}
