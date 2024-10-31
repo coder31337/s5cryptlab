b7a96435c21d5abb072f3707a3f1c01283281c30
coder31337 <coder31337@coder31337>
2024-10-31 08:38:00 +0000

    Change p8 to Accept p from user and validate it. Calculate g. Generate secret values randomly.

--- src/p8_diffie_hellman.c
+++ src/p8_diffie_hellman.c
@@ -1,29 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int power_mod(int b, int e, int m) {
-    int i, result;
-
-    result = 1;
+    int i, result = 1;
     for (i = 0; i < e; i++)
         result = (result * b) % m;
     return result;
 }
 
+int is_prime(int n) {
+    int i;
+    if (n <= 1)
+        return 0;
+    for (i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return 0;
+    return 1;
+}
+
+int get_primitive_root(int p) {
+    int g, is_primitive, i;
+    for (g = 2; g < p; g++) {
+        is_primitive = 1;
+        for (i = 1; i < p - 1; i++) {
+            if (power_mod(g, i, p) == 1) {
+                is_primitive = 0;
+                break;
+            }
+        }
+        if (is_primitive)
+            return g;
+    }
+    return -1;
+}
+
 int main() {
     int p, g, a, b, A, B, s_a, s_b;
 
-    p = 23;
-    g = 5;
-    printf("Let modulus p = %d and base g = %d\n", p, g);
+    printf("Enter a prime modulus p: ");
+    scanf("%d", &p);
+    if (!is_prime(p)) {
+        printf("Error! Invalid prime number\n");
+        return 1;
+    }
+
+    g = get_primitive_root(p);
+    if (g == -1) {
+        printf("Error! Could not find a primitive root for p\n");
+        return 1;
+    }
+    printf("Using base g = %d\n", g);
+
+    srand(time(NULL));
 
     printf("\n--- Alice ---\n");
-    a = 5;
+    a = rand() % (p - 2) + 1;
     A = power_mod(g, a, p);
     printf("Secret key is %d\n", a);
     printf("Public key is %d\n", A);
 
     printf("\n--- Bob ---\n");
-    b = 3;
+    b = rand() % (p - 2) + 1;
     B = power_mod(g, b, p);
     printf("Secret key is %d\n", b);
     printf("Public key is %d\n", B);
